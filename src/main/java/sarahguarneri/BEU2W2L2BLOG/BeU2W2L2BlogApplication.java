adc516338163ee6d08ff8dbf3726edc707bf6b4f
SarahGuarneri <sarahguarneri97@icloud.com>
2024-01-16 14:52:54 +0100

    file

---
+++ src/main/java/sarahguarneri/BEU2W2L2BLOG/BeU2W2L2BlogApplication.java
@@ -0,0 +1,13 @@
+package sarahguarneri.BEU2W2L2BLOG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W2L2BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W2L2BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sarahguarneri/BEU2W2L2BLOG/BeU2W2L2BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package sarahguarneri.BEU2W2L2BLOG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W2L2BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
